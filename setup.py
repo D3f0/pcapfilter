9735dae55df6486bce3943db9cdd330e3c8f21fd
Nahuel Defossé <nahuel.defosse@gmail.com>
2018-10-26 10:43:03 -0300

    Add tests

--- setup.py
+++ setup.py
@@ -11,11 +11,15 @@ with open("README.rst") as readme_file:
 with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
-requirements = ["Click>=6.0", "watchdog", "scapy"]
+requirements = [
+    "Click>=6.0",
+    "scapy",
+    "watchdog",
+]
 
-setup_requirements = []
+setup_requirements = ['pytest-runner', ]
 
-test_requirements = []
+test_requirements = ['pytest', 'pytest-click']
 
 setup(
     author="Nahuel Defossé",
